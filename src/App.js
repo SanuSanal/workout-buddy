71a95ee0c08eb9200321b04c9e89e35abb96ffc5
SanuSanal <=>
2024-10-06 23:02:03 +0100

    publish to pages - fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Navigate, Route, HashRouter as Router, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import WorkoutPage from './pages/WorkoutPage';
 import { WorkoutProvider } from './context/WorkoutProvider';
